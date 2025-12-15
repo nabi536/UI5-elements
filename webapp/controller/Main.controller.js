9172a189b7db43fd043296b6b17e0e03b1d5b534
Nabi saheb Inaganty <l23cse208@chalapathiengg.ac.in>
2025-12-15 05:51:17 +0000

    UI5-elements

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.nabi.project1.project1.controller.Main", {
+        onInit() {
+        }
+    });
+});
